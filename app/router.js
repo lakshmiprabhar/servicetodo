157f3c4bec7a11c9e96d81fee020cef62431fa06
lakshmiprabhar <lakshmiprabhar001@gmail.com>
2021-05-06 16:29:52 +0530

    all coomit in lakshmi before cherrypick

--- app/router.js
+++ app/router.js
@@ -6,4 +6,17 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+/*Router.map(function () {
+  this.route('list', function() {
+    this.route('add');
+    this.route('edit');
+  });
+});*/
+
+
+Router.map(function() {
+  this.route('list', function() {
+  });
+  this.route('list.add',{ path: 'list/add' });
+  this.route('list.edit');
+  });
